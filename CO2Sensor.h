5d4980c81928a31d047439f5ab6d055ca7eb71c6
Niklas Theis <39201560+TheisNiklas@users.noreply.github.com>
2021-02-26 11:48:01 +0100

    Changed Interface

--- CO2Sensor.h
+++ CO2Sensor.h
@@ -21,7 +21,7 @@ typedef enum CO2_MeasurementState_t CO2_MeasurementState_t;
 
 struct SensorData_t
 {
-	uint8_t new_data_available_u8;
+	//uint8_t new_data_available_u8;
 	float co2_value_u16;
 	float humidity_value_u16;
 	float temperature_value_u16;
@@ -44,7 +44,8 @@ CO2_Error_t CO2_InitSensor(SensorData_t *SensorData_st);
 CO2_Error_t CO2_ConfigSensor(SensorConfigData_t *SensorConfigData_st);
 CO2_Error_t CO2_StartMeasurement(void);
 CO2_Error_t CO2_StopMeasurement(void);
-CO2_Error_t CO2_UpdateData(void);
+CO2_Error_t CO2_UpdateMeasData(void);
+CO2_Error_t CO2_UpdateSensorParameterData(void);
 CO2_Error_t CO2_StartAutoCalibrationMode(void);
 CO2_Error_t CO2_StopAutoCalibrationMode(void);
 CO2_Error_t CO2_SetCO2CalibrationValue(uint16_t Co2_concentration_in_ppm);
